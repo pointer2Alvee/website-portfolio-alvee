e40ee49e2ebbbf734a2667d2f897778df3da21a1
pointer2Alvee <pointer2alvee@gmail.com>
2025-01-03 11:30:43 +0600

    #15, Minor design adjustment for mobile screen update in footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 left-0 w-full xs:h-[10%] text-center border-t pt-1 mt-3 bg-primary text-white">
-      <div className="flex flex-row justify-center items-center mt-0 mb-1 text-md">
-        <p className="text-gray-500 mr-5">
-         Developed by Alvee &copy; 2024-2025
+      <div className="flex flex-row justify-center items-center mt-0 mb-1 ">
+        <p className="text-sm text-gray-500 mr-3">
+         Developed by Alvee &copy; 2025
         </p>
         
-        <Link href="/news" className="text-accent hover:underline mx-2">
+        <Link href="/news" className="text-accent hover:underline mx-1">
           News
         </Link>
-        <Link href="/blogs" className="text-accent hover:underline mx-2">
+        <Link href="/blogs" className="text-accent hover:underline mx-1">
           Blogs
         </Link>
-        <Link href="/cv" className="text-accent hover:underline mx-2">
+        <Link href="/cv" className="text-accent hover:underline mx-1">
           Resume
         </Link>
       </div>
